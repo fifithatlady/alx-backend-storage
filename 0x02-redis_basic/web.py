424df92dd2cb09a60413ef47e4e54d08856dd210
Fifithatlady <felistasfenyere2224@gmail.com>
2024-03-29 19:24:54 +0000

    redis-basic

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
+import redis
+import requests
+from datetime import timedelta
+
+
+def get_page(url: str) -> str:
+    """
+    It uses the requests module to obtain
+    the HTML content of a particular URL and returns it.
+    Args:
+        url (str): url whose content is to be fectched
+    Returns:
+        html (str): the HTML content of the url
+    """
+    r = redis.Redis()
+    key = "count:{}{}{}".format('{', url, '}')
+    r.incr(key)
+    res = requests.get(url)
+    r.setex(url, timedelta(seconds=10), res.text)
+    return res.text
